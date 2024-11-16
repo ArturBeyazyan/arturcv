148874e5925c306f93109c3c37a4bf46718c0512
Artur5544-a <artut.123@mail.ru>
2024-11-16 13:00:47 +0400

    first commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,70 @@
-import React, {useRef, useState} from 'react'
+import {useRef, useState} from 'react'
 import "./Contact.scss"
-// import {Modal} from '../Modal/Modal'
 import emailjs from '@emailjs/browser';
 import {useTranslation} from 'react-i18next';
-import 'react-phone-input-2/lib/style.css'
+
 export const Contact = (props) => {
-    const {t} = useTranslation() 
-    const [formData,setFormData] = useState({from_name: "", from_lastName: "", from_email: "", message: "", from_phone: ""})
-    const [error,setErrors] = useState()
+    const {t} = useTranslation()
+    const [formData, setFormData] = useState({from_name: "", from_lastName: "", from_email: "",from_phone:"", message: ""})
+    const [errors,setErrors] = useState()
     const form = useRef();
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-    //     emailjs
-    //         .sendForm('service_fku2x4h', 'template_gx6b0i2', form.current, {publicKey: 'gXxhCD4Q4Zc3eP2SN'})
-    //         .then(() => {
-    //             if ('SUCCESS') {
-    //                 console.log('SUCCESS');
-    //             }
-    //         }, (error) => {
-    //             console.log('FAILED...', error.text);
-    //         },);
-    // };
+    const isValidEmail = (from_email) => {
+        const emailRegex = /^\S+@\S+\.\S+$/;
+        return emailRegex.test(from_email);
+    };
+    // const isValidPhoneNumber = (from_phone) => {
+    //     const phoneRegex = /^\d{10}$/;
+    //     return phoneRegex.test(from_phone);
+    // }
+    
+    const validateForm = () => {
+        let newError = {}
+        if (!formData.from_name) {
+            newError.from_name = "First name is required"
+        }
+        if (!formData.from_lastName) {
+            newError.from_lastName = "Last name is required"
+        }
+        if (!formData.from_email) {
+            newError.from_email = "Email is required";
+        } else if (!isValidEmail(formData.from_email)) {
+            newError.from_email = "Invalid email format";
+        }
+        if (!formData.from_phone) {
+            console.log(formData.from_phone);
+            newError.from_phone = "Phone number is required";
+        } 
+        // else if (!isValidPhoneNumber(from_phone)) {
+        //     console.log(from_phone, "PHONE");
+        //     newError.from_phone = "Phone number must be 10 digits";
+        // }
+        if (!formData.message) {
+            newError.message = "First name is required"
+        }
+        setErrors(newError);
+        return Object.keys(newError).length === 0
+    }
+    const sendEmail = (e) => {
+        emailjs
+            .sendForm('service_fku2x4h', 'template_gx6b0i2', form.current, {publicKey: 'gXxhCD4Q4Zc3eP2SN'})
+            .then(() => {
+                if ('SUCCESS') {
+                    window.location.reload(false)
+                    console.log('SUCCESS');
+                }
+            }, (error) => {
+                console.log('FAILED...', error.text);
+            },);
+    };
     const handleSubmit = (e) => {
-        // sendEmail()
+        e.preventDefault();
         const isValid = validateForm();
         if (isValid) {
-          console.log("Form Submitted", formData);
+            sendEmail(formData)
+            // window.location.reload(false)
+            console.log("Form Submitted", formData);
         } else {
-          console.log("Form Validation Failed");
+            console.log("Form Validation Failed");
         }
     }
     const handleChange = (e) => {
@@ -37,92 +74,62 @@ export const Contact = (props) => {
             [name]: value
         })
     }
-    const isValidEmail = (email) => {
-        const emailRegex = /^\S+@\S+\.\S+$/;
-        return emailRegex.test(email);
-    };
-    const isValidPhoneNumber = (from_phone) => {
-        const phoneRegex = /^\d{10}$/;
-        return phoneRegex.test(from_phone); };
-    const validateForm = (data) => {
-        let errors = {}
-        if (!data.from_name) {
-            errors.from_name = "First name is required"
-        }
-        if (!data.from_lastName) {
-            errors.from_lastName = "Last name is required"
-        }
-
-        if (!formData.from_email) {
-            errors.from_email = "Email is required";
-        } else if (!isValidEmail(formData.from_email)) {
-            errors.from_email = "Invalid email format";
-        }
-        if (!formData.from_phone) {
-            errors.from_phone = "Phone number is required";
-          } else if (!isValidPhoneNumber(formData.from_phone)) {
-            errors.from_phone = "Phone number must be 10 digits";
-          }
-        if (!data.message) {
-            errors.message = "First name is required"
-        }
-        // setErrors(errors);
-
-        return Object.keys(errors).length===0
-    }
-    console.log(error);
     return (
         <section
             className={`section contact ${props.active === 5
             ? "active"
             : ""}`}>
-            {/* <Modal Active={Active} active={active} /> */}
             <h2>{t("Contact")}</h2>
             <div className="contact_wrapper G-justify-center">
-                <form className='contact_form 'onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className='contact_form ' ref={form}>
                     <h3>{t("Together")}</h3>
                     <div className='field_box G-flex-wrap'>
                         <div className='inputBox'>
                             <input
-                                type={"text"}
-                                name="from_firstName"
-                                // required="required"
+                                type="text"
+                                name="from_name"
+                                required="required"
                                 onChange={handleChange}
                                 value={formData.from_name}/>
                             <span>{t("FName")}</span>
                             <i></i>
-                            {error.from_name && (
-                                <div className="error">{error.from_name}</div>
-                            )}
+                            {/* {errors || errors.from_name && (<div className="error">{errors.from_name}</div>)} */}
                         </div>
                         <div className='inputBox'>
                             <input
-                                type={"text"}
+                                type="text"
                                 name="from_lastName"
                                 required="required"
                                 onChange={handleChange}
                                 value={formData.from_lastName}/>
                             <span>{t("LName")}</span>
                             <i></i>
-                            {error.from_lastName && (
-                                <div className="error">{error.from_lastName}</div>
-                            )}
+                            {/* {errors.from_lastName && (<div className="error">{errors.from_lastName}</div>)} */}
                         </div>
                         <div className='inputBox'>
-                            <input required="required"onChange={handleChange}value={formData.from_phone} name="from_phone" type={"tel"}pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"/>
+
+                            <input
+                                required="required"
+                                onChange={handleChange}
+                                value={formData.from_phone}
+                                name="from_phone"
+                                type="number"/>
                             <span>{t("Phone")}</span>
                             <i></i>
-                            {error.from_phone && (
-                                <div className="error">{error.from_phone}</div>
-                            )}
+                            
                         </div>
                         <div className='inputBox'>
-                            <input required="required"name="from_email" type={"email"} onChange={handleChange}value={formData.from_email}/>
+                            <input
+                                required="required"
+                                onChange={handleChange}
+                                value={formData.from_email}
+                                name="from_email"
+                                type="text"/>
                             <span>{t("Email")}</span>
                             <i></i>
-                            {error.from_email && (
-                                <div className="error">{error.from_email}</div>
-                            )}
+                            {/* {errors && (
+                                <div className="error">{errors.from_email}</div>
+                            )} */}
                         </div>
                         <div className='inputBox inputBox_area '>
                             <textarea
@@ -132,11 +139,10 @@ export const Contact = (props) => {
                                 value={formData.message}></textarea>
                             <span>{t('YourMessage')}</span>
                             <i></i>
-                            {error.message && (
-                                <div className="error">{error.message}</div>
-                            )}
+                            {/* {errors && (
+                                <div className="error">{errors.message}</div>
+                            )} */}
                         </div>
-
                     </div>
                     <div className="contact_btn">
                         <button type='submit' className='btn'>{t("Message")}</button>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+// import React, { useEffect,useState } from 'react'
 import { About } from '../About/About'
 import { Resume } from '../Resume/Resume'
 import { Contact } from '../Contact/Contact'
