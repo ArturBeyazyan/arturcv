3f32211daecbff13b9b7301483547372a722e807
Artur5544-a <artut.123@mail.ru>
2024-12-07 11:10:43 +0400

    gfgdgfdg

--- src/components/Typewriter/Typewriter.jsx
+++ src/components/Typewriter/Typewriter.jsx
@@ -1,13 +1,15 @@
+import { useTranslation } from 'react-i18next';
 import './Typewriter.scss'
 import Typewriter from 'typewriter-effect';
 
 export const Typewriters = () => {
+  const {t}=useTranslation()
   return (
     <>
         <Typewriter options={{ autoStart: true, loop: true, }}
                 onInit={(typewriter) => {
                     typewriter.start()
-                    .typeString("Welcome My Portfolio").pauseFor(3000).deleteAll()
+                    .typeString (t("Welcome")).pauseFor(3000).deleteAll()
                     // .typeString("HTML/CSS.").pauseFor(3000).deleteAll()
                     .start()
                 }}
