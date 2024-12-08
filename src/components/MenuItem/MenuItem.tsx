cfe283975235f3a98ea1baf24836e583b5735570
Artur5544-a <artut.123@mail.ru>
2024-12-08 23:24:11 +0400

    gfgdgfdg

--- src/components/MenuItem/MenuItem.tsx
+++
@@ -1,9 +0,0 @@
-interface MenuItemProps{
-    onClick:()=>void
-    label:string;
-}
-
-const MenuItem:React.FC<MenuItemProps>=({onClick,label})=>{
-    return(<div onClick={onClick}>{label}</div>);
-}
-export default MenuItem;
