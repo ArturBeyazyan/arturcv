bfc7ece660d4f6fcba3b6136c445dfa719d470a0
Artur5544-a <artut.123@mail.ru>
2024-12-24 13:52:02 +0400

    gfgdgfdg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.scss';
-import { Errors } from './components/404Error/Errors';
+// import { Errors } from './components/404Error/Errors';
 import { Loader } from './components/Loader/Loader';
 import { Main } from './pages/Main/Main';
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-   const validPaths = ['/arturcv', '/about','/resume','/portfolio', '/contact'];
-   const currentPath = window.location.pathname;
+  //  const validPaths = ['/arturcv', '/about','/resume','/portfolio', '/contact'];
+  //  const currentPath = window.location.pathname;
   useEffect(() => {
     const fakeDataFetch = () => {
       setTimeout(() => {
@@ -17,7 +17,8 @@ function App() {
   }, []);
   return (
     <div className="App">
-      {isLoading ? (<Loader />) : (validPaths.includes(currentPath) ? (<Main />):(<Errors/>))}
+
+      {isLoading ? <Loader /> :  (<Main />)}
     </div>
   );
 }
